c4e7d565689dccb157885c0f7ee53a2790a8ed90
Renzzo131 <renzoeduardo1304@gmail.com>
2025-11-02 20:58:01 -0500

    una s

--- src/view/api-request.php
+++ src/view/api-request.php
@@ -9,7 +9,7 @@
 
 <body>
     <label for="ruta_api" class="form-label mt-3">Ruta API:</label>
-    <input type="text" class="col-12 form-control" id="ruta_api" value="http://colegios.serviciosvirtuales.com.pe/">
+    <input type="text" class="col-12 form-control" id="ruta_api" value="https://colegios.serviciosvirtuales.com.pe/">
     <form action="" id="frmApi">
         <label for="token" class="form-label mt-3">Token:</label>
         <input type="text" class="col-5 form-control" value="41aaebfb74a990aa5e7d1bb49bae390f-20251102-1" name="token" id="token">
